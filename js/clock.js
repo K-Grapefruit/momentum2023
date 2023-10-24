f8b1a9ea0993ebb5d2cb33b53d516b0637cc1b36
Full Stack Dev <50566360+K-Grapefruit@users.noreply.github.com>
2023-10-25 01:03:09 +0900

    done clock

--- js/clock.js
+++ js/clock.js
@@ -3,7 +3,12 @@ const clock = document.querySelector("#clock");
 const getClock = () => {
     const date = new Date();
 
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    //최소 길이 2글자 이상으로 만들기
+    const Hours = String(date.getHours()).padStart(2,"0");
+    const Minutes = String(date.getMinutes()).padStart(2,"0");
+    const Seconds = String(date.getSeconds()).padStart(2,"0");
+
+    clock.innerText = `${Hours}:${Minutes}:${Seconds}`;
 }
 
 getClock();
